7135e629f0c176b42a0a337ec437b0e5d074b0bb
unknown <ed.f.jones@gmail.com>
2020-05-26 13:05:18 -0400

    completed the about page

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,5 +1,17 @@
 import React from "react";
 
+
 export default function About() {
-  return <h1>hello from about page</h1>;
+  return <section className="section about-section">
+    <h1 className="section-title">About Us</h1>
+    <p>
+    I'm baby fashion axe bespoke kombucha hashtag. Meditation typewriter hammock chartreuse. 
+    Synth franzen gentrify intelligentsia la croix poutine. Pour-over wolf hot chicken selvage crucifix.
+    Flannel vice jean shorts slow-carb retro kogi. Vinyl activated charcoal hexagon normcore forage. 
+    Flexitarian coloring book church-key whatever messenger bag. Lomo neutra sustainable roof party 
+    hexagon DIY affogato VHS. Banh mi yuccie beard neutra art party, gochujang PBR&B cold-pressed. 
+    Art party cloud bread ramps letterpress lo-fi. Try-hard distillery photo booth squid salvia ugh, 
+    godard master cleanse small batch adaptogen drinking vinegar shabby chic you probably haven't heard of them.
+    </p>
+  </section>;
 }
