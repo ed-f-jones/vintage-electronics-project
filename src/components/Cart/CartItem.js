13a122f9a53d40cbf5bf3fbdb246a3c9634a3646
unknown <ed.f.jones@gmail.com>
2020-06-06 15:55:22 -0400

    increase, decrease, and remove items functions

--- src/components/Cart/CartItem.js
+++ src/components/Cart/CartItem.js
@@ -4,28 +4,27 @@ import {CartContext} from '../../context/cart';
 
 export default function CartItem({id, image, title, price, amount}) {
   // cart context
-  
+  const {removeItem, increaseAmount, decreaseAmount} = React.useContext(CartContext);
   return <article className="cart-item">
     <img src={image} alt={title}/>
     <div>
       <h4>{title}</h4>
       <h5>${price}</h5>
       <button type="button" className="cart-btn remove-btn" onClick={() => {
-        console.log("item removed")
+        removeItem(id);
       }}>
         remove
       </button>
     </div>
     <div>
       <button type="button" className="cart-btn amount-btn" onClick={()=> {
-        console.log("item increased")
-      }}><FaAngleUp/>
+      increaseAmount(id)}}><FaAngleUp/>
       </button>
 
       <p className="item-amount">{amount}</p>
 
       <button type="button" className="cart-btn amount-btn" onClick={() => {
-        console.log("item decreased")
+        decreaseAmount(id, amount);
       }}><FaAngleDown/>
       </button>
     </div>
